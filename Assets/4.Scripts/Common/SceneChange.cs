8b8ab909888d6c9bd141f8d5413d2ed9f261a609
Sa Myoung Jin <ekdskrnl105@gmail.com>
2020-01-27 16:34:14 +0900

    Scene전환 구현
    Login Scene -> LobbyScene -> BattleScene->LobbyScene .....

---
+++ Assets/4.Scripts/Common/SceneChange.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class SceneChange : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void ChangeToLoginScene()
+    {
+        SceneManager.LoadScene("Login");
+    }
+    public void ChangeToLobbyScene()
+    {
+        SceneManager.LoadScene("Lobby");
+    }
+
+    public void ChangeToBattleScene()
+    {
+        SceneManager.LoadScene("Battle");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+}
