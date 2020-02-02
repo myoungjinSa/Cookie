61b58e8829edc1bb5028d6b5cb0820ec00d1fe2d
Sa Myoung Jin <ekdskrnl105@gmail.com>
2020-02-02 15:30:46 +0900

    유니티 버전 업그레이드

---
+++ Assets/4.Scripts/Common/DataManager/GameData.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+
+namespace DataInfo
+{
+    [System.Serializable]
+    public class GameData
+    {
+        //게임 전반적인 정보를 저장할 클래스이다.
+        //ex> 
+        public float moveSpeed = 10.0f;     
+        
+    }
+
+
+
+}
